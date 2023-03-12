d36f73808b384f9fdbdfb8a10f72f005e5c6cabb
Umut Bilal Okur <umutokur11@outlook.com>
2023-03-12 13:36:18 +0300

    first commit

---
+++ src/main/java/com/ubo/schoolwebapp/SchoolWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ubo.schoolwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubo/schoolwebapp/SchoolWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubo.schoolwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
