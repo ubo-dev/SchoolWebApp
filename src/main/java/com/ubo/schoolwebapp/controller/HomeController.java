cf236e97cc3db8581c004ae85ec07a56c9620315
Umut Bilal Okur <umutokur11@outlook.com>
2023-03-12 15:02:38 +0300

    homeController

---
+++ src/main/java/com/ubo/schoolwebapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ubo.schoolwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage(){
+        return "home.html";
+    }
+}
