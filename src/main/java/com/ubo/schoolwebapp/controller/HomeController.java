124a5920fc4768b987d640d883d94dceff2a93fe
Umut Bilal Okur <umutokur11@outlook.com>
2023-03-12 20:31:37 +0300

    webpage template

--- src/main/java/com/ubo/schoolwebapp/controller/HomeController.java
+++ src/main/java/com/ubo/schoolwebapp/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.ubo.schoolwebapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage(){
+    @RequestMapping(value = {"","/","home"})
+    public String displayHomePage(Model model){
+        model.addAttribute("username","John Doe");
         return "home.html";
     }
 }
