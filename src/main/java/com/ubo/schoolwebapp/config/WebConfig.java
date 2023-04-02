8ef78ee62126603318eb50fb4972a1d8d0af2b77
Umut Bilal Okur <umutokur11@outlook.com>
2023-04-02 21:05:56 +0300

    pages

---
+++ src/main/java/com/ubo/schoolwebapp/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.ubo.schoolwebapp.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
+    }
+}
